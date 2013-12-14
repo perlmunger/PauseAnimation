47ba7b5c971befdf195aed5c1f8a54dfe5a986eb
Matt Long <matt.long@matthew-long.com>
2013-12-14 12:20:49 -0700

    Initial Commit

---
+++ PauseAnimation/MLViewController.h
@@ -0,0 +1,13 @@
+//
+//  MLViewController.h
+//  PauseAnimation
+//
+//  Created by Matt Long on 12/14/13.
+//  Copyright (c) 2013 Matt Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLViewController : UIViewController
+
+@end
